5ce81dc9695a9210443974a15dc377a3ba80f356
Karthik Rao <karthik@techjini.com>
2015-02-28 01:25:07 +0530

    Initial commit

---
+++ sampleScrollViewProject/sampleScrollViewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleScrollViewProject
+//
+//  Created by Karthik Rao on 2/28/15.
+//  Copyright (c) 2015 Techjini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleScrollViewProject/sampleScrollViewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleScrollViewProject
+//
+//  Created by Karthik Rao on 2/28/15.
+//  Copyright (c) 2015 Techjini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
